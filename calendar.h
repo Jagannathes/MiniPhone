102750dd58e755e8b31d5512ae318397c15f3027
Eldhosee <89120135+Eldhosee@users.noreply.github.com>
2021-10-30 15:36:02 +0530

    Update calendar.h

--- calendar.h
+++ calendar.h
@@ -6,7 +6,7 @@
 void calendar()
 {
 int i;
-system("clear");//change "clear "to "cls" if ur using windows os 
+system("clear");
 printf("\t\t  ##  Calendar  ## \n");
 system("cal 08 2021");
 printf("\nEnter any number to return to main menu\n");
